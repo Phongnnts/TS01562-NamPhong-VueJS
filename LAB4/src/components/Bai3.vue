c464e79a90582d3678d838e2a9230dc9c3945a01
Nam Phong <phongnnts01562@gmail.com>
2026-01-21 22:34:33 +0700

    final

---
+++ LAB4/src/components/Bai3.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container text-center mt-4">
+    <!-- Hiển thị lời chào -->
+    <h1 class="mb-3">{{ greeting }}</h1>
+
+    <!-- Ô nhập chỉnh sửa lời chào -->
+    <input
+      type="text"
+      class="form-control mb-3"
+      v-model="newGreeting"
+      placeholder="Nhập lời chào mới..."
+    />
+
+    <!-- Nút cập nhật -->
+    <button class="btn btn-primary" :title="buttonTitle" @click="changeGreeting">
+      {{ buttonText }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GreetingComponent',
+
+  data() {
+    return {
+      greeting: 'Xin chào bạn!',
+      newGreeting: '',
+      buttonTitle: 'Nhấn để cập nhật lời chào',
+      buttonText: 'Cập nhật',
+    }
+  },
+
+  methods: {
+    changeGreeting() {
+      if (this.newGreeting.trim() !== '') {
+        this.greeting = this.newGreeting
+        this.buttonTitle = 'Lời chào đã được cập nhật'
+        this.buttonText = 'Đã cập nhật'
+        this.newGreeting = ''
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 600;
+}
+</style>
---
+++ LAB4/src/components/Bai4.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container mt-5">
+    <h2 class="text-center mb-4">Danh sách bài viết</h2>
+
+    <div class="row">
+      <div class="col-md-4 mb-4" v-for="(post, index) in posts" :key="index">
+        <div class="card h-100 shadow">
+          <img :src="post.image" class="card-img-top" alt="Hình bài viết" />
+
+          <div class="card-body">
+            <h5 class="card-title">{{ post.title }}</h5>
+            <p class="card-text">{{ post.description }}</p>
+          </div>
+
+          <div class="card-footer text-center">
+            <button class="btn btn-outline-primary" @click="showAlert(post.title)">
+              Xem chi tiết
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {
+          title: 'VueJS Cơ Bản',
+          description: 'Học các khái niệm cơ bản của VueJS dễ hiểu.',
+          image: 'https://picsum.photos/400/250?random=1',
+        },
+        {
+          title: 'Bootstrap 5',
+          description: 'Thiết kế giao diện nhanh chóng và đẹp mắt.',
+          image: 'https://picsum.photos/400/250?random=2',
+        },
+        {
+          title: 'Frontend Backend',
+          description: 'Kết nối frontend với backend hiệu quả.',
+          image: 'https://picsum.photos/400/250?random=3',
+        },
+      ],
+    }
+  },
+  methods: {
+    showAlert(title) {
+      alert('Bạn đang xem bài viết: ' + title)
+    },
+  },
+}
+</script>
